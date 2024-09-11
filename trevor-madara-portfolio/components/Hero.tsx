065328014da116067a33ca1c0c401c609ac4133f
Trevor-Kayeyia-Madara <trevor.madara@student.moringaschool.com>
2024-09-11 21:27:53 +0300

    In process of doing portfolio

---
+++ trevor-madara-portfolio/components/Hero.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Spotlight } from './ui/Spotlight'
+
+const Hero = () => {
+  return (
+   <div className='pb-20 pt-36'>
+        <div>
+            <Spotlight className='pb-20'/>
+        </div>
+   </div>
+  )
+}
+
+export default Hero
--- trevor-madara-portfolio/tailwind.config.ts
+++ trevor-madara-portfolio/tailwind.config.ts
@@ -8,9 +8,12 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        black:{
+          DEFAULT: '#000',
+          100: '#000319',
+        },
       },
     },
   },
